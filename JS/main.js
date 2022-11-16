a07aa4b4dc627a7ec87a675d86a1a058060697eb
7H14G0D <thiagobrunomenezesdacosta@gmail.com>
2022-11-16 11:15:25 -0300

    JavaScript para Web - Crie páginas dinâmicas

--- JS/main.js
+++ JS/main.js
@@ -1,40 +1,18 @@
-//alert('olá mundo');
-function tocaSom (seletorAudio) {
-  const elemento = document.querySelector(seletorAudio);
-
-  if (elemento && elemento.localName === 'audio'){
-      elemento.play();
-    }
-  
-  else {
-    console.log('Elemento nao encontrado');
-  }
-
+function tocaSom (idElementoAudio) {
+    document.querySelector(idElementoAudio).play();
 }
-
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
-//for = para
-
+//para
 for (let contador = 0; contador < listaDeTeclas.length; contador++) {
-  const tecla = listaDeTeclas[contador];
-  const instrumento = tecla.classList[1];
-  const idAudio = `#som_${instrumento}`; //template string
 
-  tecla.onclick = function () {
-    tocaSom(idAudio);
-  }
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+    const idAudio = `#som_${instrumento}`; //template string
 
-  tecla.onkeydown = function (evento) {
-
-    if (evendo.code === 'Space' || evendo.code === 'Enter') {
-      tecla.classList.add('ativa');
+    tecla.onclick = function () {
+        tocaSom(idAudio);
     }
 
-  }
-
-  tecla.onkeyup= function () {
-    tecla.classList.remove('ativa');
-  }
 }
